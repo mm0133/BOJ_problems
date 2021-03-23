d3d447b9f1cb358e2e80b2cc1f2a66c438f277be
mm0133 <svs0371@naver.com>
2021-03-23 14:08:56 +0900

    2021-1 1주차

--- 2447 별찍기10.py
+++ 2447 별찍기10.py
@@ -1,15 +1,12 @@
 N=int(input())
-k=0
-while N!=3:
-    N=N//3
-    k+=1
 cur=['***','* *','***']
 def build():
     global cur
     tem1=[cur[i]*3 for i in range(len(cur))]
     tem2=[cur[i]+' '*len(cur[i])+cur[i] for i in range(len(cur))]
     cur=tem1+tem2+tem1
-for i in range(k):
+while N!=3:
+    N = N // 3
     build()
 for i in cur:
     print(i)
