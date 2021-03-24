3b73f511d5b85b39f0b7c957d1d8154424adce97
mm0133 <svs0371@naver.com>
2021-03-24 19:57:09 +0900

    2021-1 1주차

--- 2447 별찍기10.py
+++ 2447 별찍기10.py
@@ -1,3 +1,4 @@
+
 N=int(input())
 cur=['***','* *','***']
 def build():
@@ -6,7 +7,9 @@ def build():
     tem2=[cur[i]+' '*len(cur[i])+cur[i] for i in range(len(cur))]
     cur=tem1+tem2+tem1
 while N!=3:
-    N = N // 3
+    N = N / 3
     build()
-for i in cur:
-    print(i)
+print('\n'.join(cur))
+#프린트 문을 반복하는것보다 문자열을 하나로 합쳐 한번 프린트하는 것이 빠르다
+# for i in cur:
+#     print(i)
